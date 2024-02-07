dd078f82a184bfd18723386453224211ba1970e0
TippedBike89 <jgalindo-miranda25@aoiths.org>
2024-02-07 13:21:23 -0500

    5.1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public List<GameObject> targets; 
+    private float spawnRate = 1.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
     }
-
+    IEnumerator SpawnTarget()
+    {
+        while (true) 
+         {
+        yield return new WaitForSeconds(spawnRate);
+        int index = Random.Range(0, targets.Count);
+        Instantiate(targets[index]); 
+         }
+    }
     // Update is called once per frame
     void Update()
     {
