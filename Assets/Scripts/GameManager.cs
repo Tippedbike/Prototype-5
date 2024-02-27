d57f4a7e05741d6824a441c020ee9ae99bd8846d
TippedBike89 <jgalindo-miranda25@aoiths.org>
2024-02-27 12:39:05 -0500

    5.3...

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     private float spawnRate = 1.0f;
     private int score; 
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText; 
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,7 @@ public class GameManager : MonoBehaviour
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
+        gameOverText.gameObject.SetActive(true); 
     }
     IEnumerator SpawnTarget()
     {
